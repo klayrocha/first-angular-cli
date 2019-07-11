c393bfc4ab17f48ce59bf7e1ce66fc77c6f49a6a
Francis Klay Rocha <klayrocha@localhost.localdomain>
2019-07-11 15:43:48 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { User } from './user';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'first-angular-cli';
+  title = 'Tasks';
+
+  upperText:string = 'Display uppercase text';
+  lowerText:string = 'Display lowercase text';
+  percentValue:number = 0.5;
+  date:Date = new Date();
+  money: number = 598;
+  isAdmin2 : boolean = false;
+  profile: number = 2;
+  
+  user: User = {
+    name: 'Bob',
+    age: 25
+  }
 }
---
+++ src/app/task-list/task-list.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-task-list',
+  templateUrl: './task-list.component.html',
+  styleUrls: ['./task-list.component.css']
+})
+export class TaskListComponent {
+
+  tasks = [];
+  task = "";
+  isAdmin : boolean = true;
+  
+  add():void {
+    this.tasks.push(this.task);
+  }
+
+}
